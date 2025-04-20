21021f939e68a187528b17dd01242eb518df17f6
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-04-20 16:49:24 +0200

    moved files

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -5,8 +5,8 @@ import {DashboardComponent} from './admin/dashboard/dashboard.component';
 import {UnauthorizedComponent} from './util/unauthorized/unauthorized.component';
 import {HomeComponent} from './user/home/home.component';
 import {RegisterComponent} from './register/register.component';
-import {HotelSearchComponent} from './user/hotel/hotel-search/hotel-search.component';
-import {HotelDetailComponent} from './user/hotel/hotel-detail/hotel-detail.component';
+import {HotelSearchComponent} from './user/home/hotel-search/hotel-search.component';
+import {HotelDetailComponent} from './user/home/hotel-detail/hotel-detail.component';
 
 export const routes: Routes = [
     {
