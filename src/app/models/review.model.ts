118587c18d5eb7ca7b4d5fe221ff3f0bea758c9f
DenisTheMenace <denisnicovoegeli@gmail.com>
2025-04-22 13:52:20 +0200

    typos

--- src/app/models/review.model.ts
+++ src/app/models/review.model.ts
@@ -1,7 +1,3 @@
-import {Facility} from './facility.model';
-import {RoomType} from './room-type.model';
-import {Hotel} from './hotel.model';
-
 export interface Review {
   booking_id: number
   rating: number
